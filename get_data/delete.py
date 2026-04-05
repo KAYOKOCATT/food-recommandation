2a85c25bdde15a6f1c46ceb45600b1d57ee66dea
KAYOKOCATT <19848495328@163.com>
2026-04-06 00:53:03 +0800

    修改样式删除

--- get_data/delete.py
+++ get_data/delete.py
@@ -5,7 +5,7 @@ try:
         print("已成功删除food1.csv文件")
 except Exception as e:
         print(f"删除food1.csv文件失败: {str(e)}")
-    
+
 try:
         os.remove('food.csv')
         print("已成功删除food.csv文件")
